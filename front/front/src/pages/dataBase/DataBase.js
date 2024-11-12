db304fe5a55cbc1fe62294920d4a440464e7bb9e
Efrosiniya24 <efrosiniya24@gmail.com>
2024-11-12 17:50:50 +0300

    clients date base

--- front/front/src/pages/dataBase/DataBase.js
+++ front/front/src/pages/dataBase/DataBase.js
@@ -12,6 +12,7 @@ import reloadIcon from "./../../photo/reload.png";
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { FaChevronRight } from 'react-icons/fa';
+import ToursAdmin from "../toursAdmin/ToursAdmin";
 
 const DataBase = () => {
   const [users, setUsers] = useState([]);
@@ -24,6 +25,7 @@ const DataBase = () => {
   const [isVisible, setIsVisible] = useState(false);
   const [selectedUserId, setSelectedUserId] = useState(null);
   const [showOrders, setShowOrders] = useState(false);
+  const [user_id, setUser_id] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,25 +33,38 @@ const DataBase = () => {
       setError(null);
       try {
         const token = localStorage.getItem('accessToken');  
-        const response = await axios.get('http://localhost:8083/tourAgency/admin/allUsers',{
+        const response = await axios.get('http://localhost:8083/tourAgency/admin/allUsers', {
           headers: {
             'Authorization': `Bearer ${token}`,
             'Content-Type': 'application/json',
           },
         });
-          setUsers(response.data);
+        console.log(response.data); 
+        setUsers(response.data);
       } catch (error) {
-        setError(error);
+        console.error('Error fetching users:', error);
+        setError(error); 
       } finally {
         setIsLoading(false);
       }
     };
+    
     fetchData();
   }, []);
   
   const fetchUserOrders = async (userId) => {
     try {
-      const response = await axios.post('http://localhost:8000/orders/userOrders', { user_id: userId });
+      const token = localStorage.getItem('accessToken'); 
+      if (!token) {
+        throw new Error('Token not found. Please log in again.');
+      }
+      
+      const response = await axios.get(`http://localhost:8083/tourAgency/orders/getOrders/${userId}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      
       console.log(response.data); 
       setOrders(response.data);
       setShowOrders(true); 
@@ -57,6 +72,8 @@ const DataBase = () => {
       setError(error);
     }
   };
+  
+  
 
   const handleSortClick = () => {
     setSortDirection(sortDirection === 'desc' ? 'asc' : 'desc');
@@ -153,28 +170,38 @@ const DataBase = () => {
       <div className={dataBase.containerDataBase}>
         <SliderBar />
         <main className={dataBase.content}>
-          <div className={dataBase.leftPanel}>
-            <div className={dataBase.search}>
-              <img 
-                src={searchIcon} 
-                alt="Search" 
-                onClick={handleSearch}
-              />
-              <input 
-                type="text" 
-                placeholder="Введите ФИО клиента"
-                value={name}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                onKeyDown={handleSearchEnter}
-              />
-              <img 
-                className={dataBase.reloadIcon} 
-                src={reloadIcon} 
-                alt="Reload" 
-                onClick={handleReload} 
-              />
+          <div className={dataBase.searchAndUserInfo}>
+            <div className={dataBase.searchAndInfo}>
+              <div className={dataBase.search}>
+                <img 
+                  src={searchIcon} 
+                  alt="Search" 
+                  onClick={handleSearch}
+                />
+                <input 
+                  type="text" 
+                  placeholder="Введите ФИО клиента"
+                  value={name}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  onKeyDown={handleSearchEnter}
+                />
+                <img 
+                  className={dataBase.reloadIcon} 
+                  src={reloadIcon} 
+                  alt="Reload" 
+                  onClick={handleReload} 
+                />
+              </div>
             </div>
-            {isVisible && <SelectedUser user={selectedUser} dataBase={dataBase} setIsVisible={setIsVisible} fetchUserOrders={fetchUserOrders} handleCloseSelectedUser={handleCloseSelectedUser} />}
+            <div className={dataBase.userInformation}>
+            {isVisible && 
+              <SelectedUser
+              user={selectedUser} 
+              dataBase={dataBase} 
+              setIsVisible={setIsVisible} 
+              fetchUserOrders={fetchUserOrders} 
+              handleCloseSelectedUser={handleCloseSelectedUser} 
+            />}
             {showOrders && (
               <div className={dataBase.application_cards}>
                 <button className={dataBase.closeButton} onClick={handleCloseOrders}>×</button>
@@ -193,7 +220,12 @@ const DataBase = () => {
                           <div className={dataBase.user_info}>
                             <div className={dataBase.name_box}>
                               <span className={dataBase.id_label}>K</span>
-                              <span className={dataBase.name}>{`${order.user.surname} ${order.user.name} ${order.user.patronymic}`}</span>
+                              {/* Conditional rendering to prevent errors */}
+                              {order.user ? (
+                                <span className={dataBase.name}>{`${order.user.surname} ${order.user.name} ${order.user.patronymic}`}</span>
+                              ) : (
+                                <span className={dataBase.name}>User information not available</span>
+                              )}
                             </div>
                           </div>
                         </div>
@@ -204,67 +236,87 @@ const DataBase = () => {
                                 <span className={dataBase.date}>{new Date(order.createdAt).toLocaleDateString()}</span>
                               </div>
                               <div className={dataBase.name_box}>
-                                <span className={dataBase.id_label}>email</span>
-                                <span className={dataBase.name}>{order.user.email}</span>
+                                <span className={dataBase.id_label}>Email</span>
+                                {/* Check if user exists before trying to access email */}
+                                {order.user ? (
+                                  <span className={dataBase.name}>{order.user.email}</span>
+                                ) : (
+                                  <span className={dataBase.name}>Email not available</span>
+                                )}
                               </div>
                             </div>
                             <div className={dataBase.phone_box}>
-                              <span className={dataBase.phone}>{order.user.phone}</span>
+                              {/* Check if user exists before trying to access phone */}
+                              {order.user ? (
+                                <span className={dataBase.phone}>{order.user.phone}</span>
+                              ) : (
+                                <span className={dataBase.phone}>Phone not available</span>
+                              )}
                             </div>
                             <div className={dataBase.inline_container}>
                               <div className={dataBase.phone_box}>
                                 <span className={dataBase.id_label}>Кол. чел.:</span>
                                 <span className={dataBase.phone}>{order.numberOfPeople}</span>
                               </div>
-                              <div className={dataBase.phone_box}>
-                                <span className={dataBase.id_label}>Турагентство:</span>
-                                <span className={dataBase.phone}>{order.tour.travelAgency}</span>
-                              </div>
                             </div>
                           </div>
                           <div className={dataBase.application_main}>
                             <div className={dataBase.application_details}>
-                              <p className={`${dataBase.header} ${dataBase.rectangle}`}>{order.tour.name}</p>
-                              <div>
-                                <p>{order.tour.description}</p>
-                              </div>
+                              {/* Ensure order.tour is defined before rendering its properties */}
+                              {order.tour ? (
+                                <>
+                                  <p className={`${dataBase.header} ${dataBase.rectangle}`}>{order.tour.name}</p>
+                                  <div>
+                                    <p>{order.tour.description}</p>
+                                  </div>
+                                </>
+                              ) : (
+                                <p>Tour information not available</p> 
+                              )}
                               <button className={dataBase.nextButton}><FaChevronRight /></button>
                             </div>
                           </div>
+                          
                         </div>
                       </div>
                     );
                   })
                 )}
-              </div>
+            </div>
             )}
           </div>
-
-          <section className={dataBase.data}>
+          </div>
+          <div className={dataBase.data}>
             <table className={dataBase.tableDataBase}>
               <thead>
-                <tr>
-                  <th className={dataBase.th_content}>
+                <tr className={dataBase.first}>
+                  <th className={dataBase.idColumn}>
                     <div>ID</div>
                     <button onClick={handleSortClick} className={dataBase.sort_button}>
                       <img src={sortDirection === 'asc' ? up : down} alt="Sort direction" className={dataBase.sort_icon} /> 
                     </button>
                   </th>
-                  <th className={dataBase.th_content}><div>Клиент</div></th>
+                  <th className={dataBase.clientColumn}>Клиент</th>
                 </tr>
               </thead>
               <tbody>
                 {sortedUsers.map((user) => (
-                  <tr key={user.id} onClick={() => handleUserClick(user)}>
-                    <td>{user.id}</td>
-                    <td>{user.surname} {user.name} {user.patronymic}</td>
-                  </tr>
+                  user && user.surname ? (
+                    <tr key={user.id} onClick={() => handleUserClick(user)}>
+                      <td className={dataBase.idColumn}>{user.id}</td>
+                      <td className={dataBase.clientColumn}>{user.surname} {user.name} {user.patronymic}</td>
+                    </tr>
+                  ) : (
+                    <tr key={user?.id || Math.random()}><td colSpan="2">Invalid User Data</td></tr>
+                  )
                 ))}
               </tbody>
             </table>
-          </section>
+          </div>
+          
         </main>
       </div>
+      
     </div>
   );
 }
