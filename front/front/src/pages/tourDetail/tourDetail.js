92442e4fffe0c361139f213906042e76f3991a29
Efrosiniya24 <efrosiniya24@gmail.com>
2024-11-19 02:50:05 +0300

    navigation

--- front/front/src/pages/tourDetail/tourDetail.js
+++ front/front/src/pages/tourDetail/tourDetail.js
@@ -1,73 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'; 
+import { NavLink, useLocation } from 'react-router-dom';
+import HeaderClient from '../../components/headerClient/headerClient';
 import styles from './tourDetail.module.css';
+import axios from 'axios';
 
 const TourDetail = () => {
-    const { id } = useParams();
-    const [tour, setTour] = useState(null);
-    const navigate = useNavigate();
+    const location = useLocation();
+    const tour = location.state?.tour;
+    const [photos, setPhotos] = useState([null, null, null, null]);
+    const [tourDetails, setTourDetails] = useState(null);
 
     useEffect(() => {
-        // Fetch tour details from API
-        axios.get(`http://localhost:8000/tours/viewingTour/${id}/`)
-            .then(response => {
-                setTour(response.data);
-            })
-            .catch(error => {
-                console.error('Error fetching tour details:', error);
-            });
-    }, [id]);
+        if (tour) {
+            axios.get(`http://localhost:8083/tourAgency/tours/tour/${tour.id}`)
+                .then(response => {
+                    setTourDetails(response.data); 
+                    fetchPhotos(response.data);
+                })
+                .catch(error => console.error('Error fetching tour details:', error));
+        }
+    }, [tour]);
 
-    const handleBookClick = () => {
-        navigate('/application', { state: { tour } });
+    const fetchPhotos = (tour) => {
+        axios.get(`http://localhost:8083/tourAgency/photo/getPhoto/${tour.id}`, { withCredentials: true })
+            .then(response => {
+                console.log("Фотографии:", response.data);
+                if (Array.isArray(response.data)) {
+                    const loadedImages = response.data.map((photoData, index) => {
+                        if (photoData && photoData.trim() !== '') {
+                            return `data:image/jpeg;base64,${photoData}`;
+                        }
+                        return null;
+                    });
+
+                    setPhotos(loadedImages.concat(Array(4 - loadedImages.length).fill(null))); 
+                } else {
+                    console.error("Ошибка: Ответ от сервера не является массивом изображений");
+                }
+            })
+            .catch(error => console.error("Ошибка загрузки фотографий:", error));
     };
 
-    if (!tour) {
-        return <div>Loading...</div>;
-    }
-
+    const formatText = (text) => {
+        return text.replace(/\n/g, '<br />');
+    };
+    
     return (
-        <div className={styles.tourDetailPage}>
-            <div className={styles.tourDetailContainer}>
-                <div className={styles.detailsContainer}>
-                    <h1 className={styles.title}>{tour.name}</h1>
-                    <p className={styles.description}>{tour.description}</p>
-                    <div className={styles.info}>
-                        <div className={styles.infoItem}>
-                            <span className={styles.label}>Страна:</span>
-                            <span className={styles.value}>{tour.country}</span>
-                        </div>
-                        <div className={styles.infoItem}>
-                            <span className={styles.label}>Город:</span>
-                            <span className={styles.value}>{tour.city}</span>
-                        </div>
-                        <div className={styles.infoItem}>
-                            <span className={styles.label}>Количество дней:</span>
-                            <span className={styles.value}>{tour.numberOfDays}</span>
-                        </div>
-                        <div className={styles.infoItem}>
-                            <span className={styles.label}>Стоимость:</span>
-                            <span className={styles.value}>${tour.price}</span>
-                        </div>
-                        <div className={styles.infoItem}>
-                            <span className={styles.label}>Начальная дата:</span>
-                            <span className={styles.value}>{new Date(tour.beginningDate).toLocaleDateString()}</span>
-                        </div>
-                        <div className={styles.infoItem}>
-                            <span className={styles.label}>Дата окончания:</span>
-                            <span className={styles.value}>{new Date(tour.endDate).toLocaleDateString()}</span>
-                        </div>
-                        <div className={styles.infoItem}>
-                            <span className={styles.label}>Туристическое агентство:</span>
-                            <span className={styles.value}>{tour.travelAgency}</span>
+        <div>
+            <HeaderClient/>
+            <div className={styles.tourDetailPage}>
+                <div className={styles.highLink}>
+                    <ul>
+                        <li>
+                            <NavLink to="/" className={styles.noLink}>Главная</NavLink>
+                        </li> 
+                        <li>
+                            <NavLink to="/tours" className={styles.noLink}>/Каталог</NavLink>   
+                        </li>
+                        <li> /{tourDetails?.name}</li>
+                    </ul>
+                </div>
+
+                <div className={styles.tour}>
+                    <div className={styles.leftPart}>
+                        <div className={styles.photosTour}>
+                            {photos.map((photo, index) => (
+                                <img
+                                    key={index}
+                                    src={photo}
+                                    alt={`Фото ${index + 1}`}
+                                />
+                            ))}
                         </div>
                     </div>
-                    <div className={styles.programContainer}>
-                        <h2 className={styles.programTitle}>Программа</h2>
-                        <p className={styles.program}>{tour.program}</p>
+
+                    <div className={styles.rightPart}>
+                        <div className={styles.aboutTourUp}>
+                            <h1>{tour.name}</h1>
+                            <p dangerouslySetInnerHTML={{ __html: formatText(tour.description) }} />
+                        </div>
                     </div>
-                    <button className={styles.bookButton} onClick={handleBookClick}>Оформить заявку</button>
                 </div>
             </div>
         </div>
