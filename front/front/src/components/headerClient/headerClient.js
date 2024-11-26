8b8f1f2a86ce3b19b813eea5edb02bfda7a8cdbd
Efrosiniya24 <efrosiniya24@gmail.com>
2024-11-27 00:11:31 +0300

    client header + logout

--- front/front/src/components/headerClient/headerClient.js
+++ front/front/src/components/headerClient/headerClient.js
@@ -1,9 +1,41 @@
 import React from 'react';
 // import header from "./headerAdmin.module.css";
 import header from "./headerClient.module.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import logoutPhoto from './logout.png';
+import { useAuth } from '../../components/AuthProvider';
+import axios from 'axios';
 
 const HeaderClient = () => {
+    const { user, logout } = useAuth(); 
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            const token = localStorage.getItem('authToken');
+
+            const response = await axios.post(
+                'http://localhost:8083/tourAgency/auth/logout', 
+                {}, 
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`, 
+                    }
+                }
+            );
+
+            if (response.status === 200) {
+                logout(); 
+
+                navigate('/');
+            } else {
+                throw new Error('Logout failed');
+            }
+        } catch (error) {
+            console.error('Logout error:', error);
+        }
+    };
+
     return ( 
         <header className={header.headerNotMain}>
         <NavLink to ="/" className={header.logo}>WORLD</NavLink>
@@ -13,10 +45,20 @@ const HeaderClient = () => {
                 <li>О нас</li>
                 <li>Туры</li>
                 <li>Профиль</li>
+                {user && (
+                        <li>
+                            <img 
+                                src={logoutPhoto} 
+                                alt="Logout" 
+                                onClick={handleLogout} 
+                                className={header.logoutIcon}
+                            />
+                        </li>
+                    )}
             </ul>
         </div>
         </header>
-     );
+    );
 }
  
 export default HeaderClient;
