19ca163def3d22feaf84b45506b515a0b587a539
Efrosiniya24 <efrosiniya24@gmail.com>
2025-04-07 23:52:10 +0300

    .

--- front/front/src/pages/mainPage/MainPageUser.js
+++ front/front/src/pages/mainPage/MainPageUser.js
@@ -1,6 +1,8 @@
-import header from "./../../styles/header.module.css"
+
+import header from "./header.module.css"
 import index from "./index.module.css"
 import video from "./beach.mp4"
+import forAllPages from "./../../styles/forAllPAges.module.css";
 
 import { NavLink } from "react-router-dom"
 
@@ -31,5 +33,43 @@ const MainPageUser = () => {
     </div>
   );
 }
- 
 export default MainPageUser;
+ 
+// export default MainPageUser;
+// import index from "./index.module.css"
+// import video from "./beach.mp4"
+// import forAllPages from "./../../styles/forAllPAges.module.css";
+
+// import { NavLink } from "react-router-dom"
+
+// const MainPageUser = () => {
+//     return ( 
+//             <div>
+//             <div class= "headerMain">
+//             <a href="/static" class="logo">WORLD</a>
+//             <ul class="nav">
+//                 <li><a href="#">Главная</a></li>
+//                 {/* <li><NavLink to ="/aboutUs"> О нас</NavLink></li>
+//                 <li><NavLink to ="/tours">Туры</NavLink></li> */}
+//             </ul>
+//         </div>
+
+
+//         <div className={header.intro}>
+//             <div className={index.video}>
+//                 <video className={index.video_media} src={video} autoPlay muted loop></video>
+//             </div>
+//             <div className={header.intro_text}>
+//                 <h1 className={index.h1_index}>WORLD</h1>
+//                 <p className={index.mainP}>Вдохновение в путешествиях</p>
+//                 <div className={index.buttons}>
+//                     <NavLink to ="/tours">Посмотреть туры</NavLink>
+//                     <NavLink to="/signIn">Войти</NavLink>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//   );
+// }
+ 
+// export default MainPageUser;
